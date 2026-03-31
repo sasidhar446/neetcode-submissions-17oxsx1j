bf6b2c8ab66139e177fdc66d35fd923c616ffe08
sasidhar446 <sasidhar446@gmail.com>
2026-03-31 21:51:34 +0530

    Add: permutation-string - submission-6

---
+++ Data Structures & Algorithms/permutation-string/submission-6.py
@@ -0,0 +1,20 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        n1, n2 = len(s1), len(s2)
+        if n1 > n2:
+            return False
+        s1_freq = Counter(s1)
+        w_freq = Counter(s2[:n1])
+        if s1_freq == w_freq:
+            return True
+        for i in range(n1, n2):
+            char_in = s2[i]
+            char_out = s2[i - n1]
+            w_freq[char_in] += 1
+            w_freq[char_out] -=1
+            if w_freq[char_out] == 0:
+                del w_freq[char_out]
+            if s1_freq == w_freq:
+                return True 
+        return False
+        
