463767b448372dd1cca9ab5081beeb965d04ce0b
sasidhar446 <sasidhar446@gmail.com>
2026-04-02 19:55:12 +0530

    Update: validate-parentheses - submission-12

--- Data Structures & Algorithms/validate-parentheses/submission-12.py
+++ Data Structures & Algorithms/validate-parentheses/submission-12.py
@@ -2,15 +2,16 @@ class Solution:
     def isValid(self, s: str) -> bool:
         if len(s) <= 1:
             return False
-        lookup = {']': '[', '}': '{', ')': '('}
+        open_ = {']': '[', '}': '{', ')': '('}
         stack = []
         for c in s:
-            if c in lookup:
-                if stack and stack[-1] == lookup[c]:
+            if c in open_:
+                if stack and stack[-1] == open_[c]:
                     stack.pop()
                 else:
                     return False
             else:
                 stack.append(c)
         return not stack
+
         
