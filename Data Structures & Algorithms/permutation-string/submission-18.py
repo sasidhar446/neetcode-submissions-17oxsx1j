bb4b0e4e67ec60de1dfddc34acd7408bb3af24d7
sasidhar446 <sasidhar446@gmail.com>
2026-05-21 11:29:20 +0530

    Add: permutation-string - submission-18

---
+++ Data Structures & Algorithms/permutation-string/submission-18.py
@@ -0,0 +1,23 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s2) < len(s1):
+            return False
+        need = Counter(s1)
+        left, right = 0, len(s1)
+        have = Counter(s2[left:right])
+        while right < len(s2):
+            if need == have:
+                return True
+            else:
+                have[s2[right]] += 1
+                have[s2[left]] -= 1
+                if have[s2[left]] == 0:
+                    del have[s2[left]]
+                left += 1
+                right += 1
+        return need == have
+
+
+            
+    
+        
