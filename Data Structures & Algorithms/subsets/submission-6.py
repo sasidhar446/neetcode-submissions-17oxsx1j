1e7580087ccf7002fa1b95e058ec4178acbbc9a9
sasidhar446 <sasidhar446@gmail.com>
2026-06-08 13:26:49 +0530

    Add: subsets - submission-6

---
+++ Data Structures & Algorithms/subsets/submission-6.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result, current = [], []
+
+        def backtrack(index):
+            result.append(current[:])
+            
+            for i in range(index, len(nums)):
+                current.append(nums[i])
+                backtrack(i + 1)
+                current.pop()
+        
+        backtrack(0)
+
+        return result
