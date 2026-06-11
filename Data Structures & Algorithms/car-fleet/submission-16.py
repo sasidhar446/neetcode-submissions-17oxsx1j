200f67a3d9cd7408894152c8da1d0f9c8a31485c
sasidhar446 <sasidhar446@gmail.com>
2026-06-11 09:20:50 +0530

    Add: car-fleet - submission-16

---
+++ Data Structures & Algorithms/car-fleet/submission-16.py
@@ -0,0 +1,23 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        fleets = [0] * len(position)
+        for index, result in enumerate(sorted(zip(position, speed), reverse=True)):
+            fleets[index] = (target - result[0]) / result[1]
+        
+        count = 1
+        current_fleet = fleets[0]
+        
+        for fleet in fleets:
+            if fleet > current_fleet:
+                count += 1
+                current_fleet = fleet
+        
+        return count
+
+
+        
+            
+
+
+
+            
