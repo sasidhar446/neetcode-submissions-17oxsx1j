7171f4cc22c5bd1eeae9cb805329235b23390048
sasidhar446 <sasidhar446@gmail.com>
2026-06-18 11:04:58 +0530

    Add: pacific-atlantic-water-flow - submission-1

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-1.py
@@ -0,0 +1,52 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        pacific_visited, atlantic_visited = set(), set()
+        pacific_deq = collections.deque()
+        atlantic_deq = collections.deque()
+        rows, cols, result = len(heights), len(heights[0]), []
+
+        for r in range(rows):
+            for c in range(cols):
+                if r == 0 or c == 0:
+                    pacific_deq.append((r, c))
+                    pacific_visited.add((r, c))
+                if r == rows - 1 or c == cols - 1:
+                    atlantic_deq.append((r, c))
+                    atlantic_visited.add((r, c))
+        
+        while pacific_deq:
+            r, c = pacific_deq.popleft()
+
+            for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                nr, nc = dr + r, dc + c
+
+                if 0 <= nr < rows and 0 <= nc < cols and heights[nr][nc] >= heights[r][c]:
+                    if (nr, nc) not in pacific_visited:
+                        pacific_deq.append((nr, nc))
+                        pacific_visited.add((nr, nc))
+
+        
+        while atlantic_deq:
+            r, c = atlantic_deq.popleft()
+
+            for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                nr, nc = dr + r, dc + c
+
+                if 0 <= nr < rows and 0 <= nc < cols and heights[nr][nc] >= heights[r][c]:
+                    if (nr, nc) not in atlantic_visited:
+                        atlantic_deq.append((nr, nc))
+                        atlantic_visited.add((nr, nc))
+
+        
+        for cell in atlantic_visited:
+            if cell in pacific_visited:
+                result.append(cell)
+
+        return result
+
+
+
+
+
+
+
