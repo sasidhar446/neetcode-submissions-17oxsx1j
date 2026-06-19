4a26e6d5a6b80506fa6e8dcd7258d74bfb8a05f4
sasidhar446 <sasidhar446@gmail.com>
2026-06-19 11:53:58 +0530

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.py
@@ -0,0 +1,30 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adjList = defaultdict(list)
+        visit = [0] * numCourses
+
+        for course, prereq in prerequisites:
+            adjList[prereq].append(course)
+
+        def has_cycle(course):
+            if visit[course] == 1:
+                return True
+            if visit[course] == 2:
+                return False
+            
+            visit[course] = 1
+
+            for neighbour in adjList[course]:
+                if has_cycle(neighbour):
+                    return True
+                    break
+            
+            visit[course] = 2
+
+            return False
+        
+        for course in range(numCourses):
+            if has_cycle(course):
+                return False
+        
+        return True
