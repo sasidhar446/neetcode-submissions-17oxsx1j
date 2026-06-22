db446fc1a9003ed2c890bbb38ed10909bc838b1f
sasidhar446 <sasidhar446@gmail.com>
2026-06-22 14:26:55 +0530

    Add: combinations-of-a-phone-number - submission-3

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-3.py
@@ -0,0 +1,31 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+
+        result, current = [], []
+
+        hashmap = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+
+        def backtrack(index):
+            if index == len(digits):
+                result.append("".join(current[:]))
+                return
+
+            for char in hashmap[digits[index]]:
+                current.append(char)
+                backtrack(index + 1)
+                current.pop()
+
+        backtrack(0)
+        
+        return result
