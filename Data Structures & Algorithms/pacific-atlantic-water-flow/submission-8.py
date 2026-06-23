caf7b9fe8260858fc7437939e0aa1061590984d0
sasidhar446 <sasidhar446@gmail.com>
2026-06-23 14:43:34 +0530

    Add: pacific-atlantic-water-flow - submission-8

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-8.py
@@ -0,0 +1,36 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+
+        pacific_set, atlantic_set, pacific_deq, atlantic_deq = set(), set(), deque(), deque()
+
+        rows, cols, result = len(heights), len(heights[0]), []
+
+        for r in range(rows):
+            for c in range(cols):
+                if r == 0 or c == 0:
+                    pacific_deq.append((r, c))
+                    pacific_set.add((r, c))
+                if r == rows - 1 or c == cols -1:
+                    atlantic_deq.append((r, c))
+                    atlantic_set.add((r, c))
+        
+        def bfs(deq_, set_):
+            while deq_:
+                r, c = deq_.popleft()
+                for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                    nr, nc = dr + r, dc + c
+                    if 0 <= nr < rows and 0 <= nc < cols and heights[nr][nc] >= heights[r][c] and (nr, nc) not in set_:
+                        deq_.append((nr, nc))
+                        set_.add((nr, nc))
+        
+        bfs(pacific_deq, pacific_set)
+        bfs(atlantic_deq, atlantic_set)
+
+        for r, c in pacific_set:
+            if (r, c) in atlantic_set:
+                result.append([r, c])
+        
+        return result
+
+                        
+        
