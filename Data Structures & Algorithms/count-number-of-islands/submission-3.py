c15de5d65f14407176c007ccfaf56fba88b239c7
sasidhar446 <sasidhar446@gmail.com>
2026-06-23 09:12:06 +0530

    Add: count-number-of-islands - submission-3

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-3.py
@@ -0,0 +1,27 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        rows, cols, islands = len(grid), len(grid[0]), 0
+        deq = deque()
+
+        def bfs():
+            r, c = deq.popleft()
+
+            grid[r][c] = '0'
+
+            for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                nr, nc = dr + r, dc + c
+
+                if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == '1':
+                    deq.append((nr, nc))
+                    bfs()
+
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == '1':
+                    islands += 1
+                    deq.append((r, c))
+                    bfs()
+        
+        return islands
+        
