bc7f724c27d97904b342b4e36a834af727a7ad15
sasidhar446 <sasidhar446@gmail.com>
2026-09-13 12:26:52 +0530

    Add: reorder-linked-list - submission-5

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-5.py
@@ -0,0 +1,40 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        slow, fast = head, head
+
+        # step 1 : To identify the mid point
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # step 2 : To reverse the second half
+
+        curr, prev = slow, None
+
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        
+        # step 3 : Merge two linked list now
+
+        first, second = head, prev
+
+        while first and second.next:
+            tmp1, tmp2 = first.next, second.next
+            first.next, second.next = second, tmp1
+            first, second = tmp1, tmp2
+        
+
+
+
+
+        
+        
